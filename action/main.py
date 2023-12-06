cef8f5c9e5840c0e2713adaf89a181ffaee13975
CalebTVanDyke <ctvandyke24@gmail.com>
2023-12-05 20:05:12 -0600

    remove unused code

--- action/main.py
+++ action/main.py
@@ -11,13 +11,6 @@ DKEY = os.environ["INPUT_DKEY"]
 WORKING_DIR = os.environ["INPUT_WORKING_DIR"]
 CLI_VERSION = os.getenv("INPUT_CLI_VERSION", None)
 
-version_specifier = ""
-if VERSION != "":
-    if not re.match(r"v?\d\.\d{1,3}\.\d{1,3}$", VERSION):
-        print("VERSION does not match expected pattern")
-        sys.exit(1)
-    version_specifier = f"=={VERSION}"
-
 if CLI_VERSION is not None:
     cmd = f"pip install launchflow=={CLI_VERSION}"
 else:
